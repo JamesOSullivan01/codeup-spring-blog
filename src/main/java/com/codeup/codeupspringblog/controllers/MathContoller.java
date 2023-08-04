6af621cd3808bf7eaff4d8644928664ab7c2041d
James OSullivan <james.a.osullivan01@gmail.com>
2023-08-04 15:56:23 -0500

    Fixed gitignore

---
+++ src/main/java/com/codeup/codeupspringblog/controllers/MathContoller.java
@@ -0,0 +1,24 @@
+package com.codeup.codeupspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathContoller {
+
+    @GetMapping("/add/{first_number}/and/{second_number}")
+    @ResponseBody
+    public int addition(@PathVariable int first_number,
+                        @PathVariable int second_number) {
+        return first_number + second_number;
+    }
+    @GetMapping("/subtract/{first_number}/from/{second_number}")
+    @ResponseBody
+    public int subtraction(@PathVariable int first_number,
+                        @PathVariable int second_number) {
+        return first_number + second_number;
+    }
+
+}
