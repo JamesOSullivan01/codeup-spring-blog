bd8c36e1f6563ec6f02e371b8834716a397b6ea5
James OSullivan <james.a.osullivan01@gmail.com>
2023-08-17 16:59:35 -0500

    Added testing dependencies

---
+++ src/test/java/com/codeup/codeupspringblog/PostsIntegrationTests.java
@@ -0,0 +1,123 @@
+package com.codeup.codeupspringblog;
+
+import com.codeup.codeupspringblog.models.Post;
+import com.codeup.codeupspringblog.models.User;
+import com.codeup.codeupspringblog.repositories.PostRepository;
+import com.codeup.codeupspringblog.repositories.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.nio.file.Path;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CodeupSpringBlogApplication.class)
+@AutoConfigureMockMvc
+
+
+public class PostsIntegrationTests {
+
+    private User testUser;
+    private HttpSession httpSession;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    UserRepository userDao;
+
+    @Autowired
+    PostRepository postDao;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Before
+    public void setup() throws Exception {
+
+        testUser = userDao.findByUsername("user3");
+
+        // Creates the test user if not exists
+        if(testUser == null){
+            User newUser = new User();
+            newUser.setUsername("user3");
+            newUser.setPassword(passwordEncoder.encode("12345"));
+            newUser.setEmail("testUser@codeup.com");
+            testUser = userDao.save(newUser);
+        }
+
+            httpSession = this.mvc.perform(post("/login").with(csrf())
+                            .param("username", "user3")
+                            .param("password", "12345"))
+                    .andExpect(status().is(HttpStatus.FOUND.value()))
+                    .andExpect(redirectedUrl("/posts"))
+                    .andReturn()
+                    .getRequest()
+                    .getSession();
+
+        // Throws a Post request to /login and expect a redirection to the Ads index page after being logged in
+    }
+
+    @Test
+    public void contextLoads() {
+        // Sanity Test, just to make sure the MVC bean is working
+        assertNotNull(mvc);
+    }
+
+    @Test
+    public void testIfUserSessionIsActive() throws Exception {
+        // It makes sure the returned session is not null
+        assertNotNull(httpSession);
+    }
+
+    @Test
+    public void testCreateAd() throws Exception {
+        // Makes a Post request to /ads/create and expect a redirection to the Ad
+        this.mvc.perform(
+                        post("/posts/create").with(csrf())
+                                .session((MockHttpSession) httpSession)
+                                // Add all the required parameters to your request like this
+                                .param("title", "test")
+                                .param("body", "for sale"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+
+    @Test
+    public void testShowAd() throws Exception {
+
+        Post existingPost = postDao.findAll().get(0);
+
+        // Makes a Get request to /ads/{id} and expect a redirection to the Ad show page
+        this.mvc.perform(get("/posts/show/" + existingPost.getId())
+                        .with(csrf())
+                        .session((MockHttpSession) httpSession))
+                .andExpect(status().isOk())
+
+                .andExpect((ResultMatcher) content().string(containsString(existingPost.getBody())));
+    }
+
+
+
+
+}
+
